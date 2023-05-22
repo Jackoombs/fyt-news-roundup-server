3fd2b89c648426679242f532ccfa847fdb1a9790
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-05-22 12:08:27 +0100

    Updated app module file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,47 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { BrowserService } from './scrape/browser/browser.service';
+import { ScrapeManagerService } from './scrape/scrape-manager/scrape-manager.service';
+import { ArticleService } from './article/article.service';
+import { OutletService } from './outlet/outlet.service';
+import { CategoryService } from './category/category.service';
+import { PrismaService } from './prisma/prisma.service';
+import { ArticleModule } from './article/article.module';
+import { OutletModule } from './outlet/outlet.module';
+import { CategoryModule } from './category/category.module';
+import { join } from 'path';
+import { ScrapeArticlesService } from './scrape/scrape-articles/scrape-articles.service';
+import { BbcArticleScraperService } from './scrape/scrape-articles/bbc-article-scraper/bbc-article-scraper.service';
+import { ScraperUtilsService } from './scrape/scraper-utils/scraper-utils.service';
+import { stopWords } from './scrape/scraper-utils/stopwords';
+import { ThedailymailArticleScraperService } from './scrape/scrape-articles/thedailymail-article-scraper/thedailymail-article-scraper.service';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
     }),
+    ArticleModule,
+    OutletModule,
+    CategoryModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    BrowserService,
+    ScrapeManagerService,
+    ArticleService,
+    OutletService,
+    CategoryService,
+    PrismaService,
+    ScrapeArticlesService,
+    BbcArticleScraperService,
+    ScraperUtilsService,
+    { provide: 'STOPWORDS', useValue: stopWords },
+    ThedailymailArticleScraperService,
+  ],
 })
 export class AppModule {}
