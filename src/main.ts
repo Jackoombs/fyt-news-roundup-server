0d02199ca912aedecbec451cc68259d95ae401f0
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-05-22 11:56:53 +0100

    Added cors options.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
+
+  const corsOptions: CorsOptions = {
+    origin: ['https://www.jacobs.properties', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
 }
+
 bootstrap();
