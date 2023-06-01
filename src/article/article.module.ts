38c56ef1b31fb9c98371af3c6df3e3e22e2fe283
Jackoombs <jack.coombs@hotmail.co.uk>
2023-06-01 11:16:00 +0100

    Added dependant modules.

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleResolver } from './article.resolver';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { KeywordsService } from 'src/keywords/keywords.service';
+import { ScraperUtilsService } from 'src/scrape/scraper-utils/scraper-utils.service';
+import { stopWords } from 'src/scrape/scraper-utils/stopwords';
 
 @Module({
-  providers: [ArticleResolver, ArticleService, PrismaService],
+  providers: [
+    ArticleResolver,
+    ArticleService,
+    PrismaService,
+    KeywordsService,
+    ScraperUtilsService,
+    { provide: 'STOPWORDS', useValue: stopWords },
+  ],
 })
 export class ArticleModule {}
