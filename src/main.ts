2696c182c3d628376c67671c8dd12e57bf3336c9
Jackoombs <jack.coombs@hotmail.co.uk>
2023-06-01 11:15:12 +0100

    Fixed port issue.

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,15 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 8080);
-
   const corsOptions: CorsOptions = {
-    origin: ['https://www.jacobs.properties', 'http://localhost:3000'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   };
 
   app.enableCors(corsOptions);
+  await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
